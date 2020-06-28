f3825beb036d32448ed4613611d613788c1fa8f5
feiyangmeiyu <806567171@qq.com>
2020-06-28 15:14:10 +0200

    add the job

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+	title = models.CharField(max_length=225)
+	pub_date = models.DateTimeField()
+	body = models.TextField(max_length=1000)
+	image = models.ImageField(upload_to='images/')
+# Create a blog model
+# title
+# pub_date
+# body
+#
+
+
+# Add the Blog app to the settings
+
+#Create a migration
+
+#Migrate
+
+#Add to the admin
