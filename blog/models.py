12e6aa2c75804d947b10c3462f94a376b81f4113
feiyangmeiyu <806567171@qq.com>
2020-06-28 18:42:22 +0200

    Add the blog page

--- blog/models.py
+++ blog/models.py
@@ -5,13 +5,15 @@ class Blog(models.Model):
 	pub_date = models.DateTimeField()
 	body = models.TextField(max_length=1000)
 	image = models.ImageField(upload_to='images/')
-# Create a blog model
-# title
-# pub_date
-# body
-#
 
+	def __str__(self):
+		return self.title
 
+	def summary(self):
+		return self.body[:100]
+
+	def pub_date_pretty(self):
+		return self.pub_date.strftime('%b %e %Y')
 # Add the Blog app to the settings
 
 #Create a migration
